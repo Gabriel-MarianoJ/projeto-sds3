4c5754675779dae62ff3eceb1d22202bda543a54
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2022-01-30 16:36:37 -0300

    Camadas

---
+++ backend/src/main/java/com/gabrielm/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielm.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielm.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/gabrielm/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.gabrielm.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielm.dsvendas.dto.SellerDTO;
+import com.gabrielm.dsvendas.entities.Seller;
+import com.gabrielm.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
