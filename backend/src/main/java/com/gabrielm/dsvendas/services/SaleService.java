9e4ca764f68b239b84b5a161810efe3d00aaca80
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2022-01-30 17:48:44 -0300

    Adicionado buscas agrupadas

--- backend/src/main/java/com/gabrielm/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/gabrielm/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.gabrielm.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gabrielm.dsvendas.dto.SaleDTO;
+import com.gabrielm.dsvendas.dto.SaleSuccessDTO;
+import com.gabrielm.dsvendas.dto.SaleSumDTO;
 import com.gabrielm.dsvendas.entities.Sale;
 import com.gabrielm.dsvendas.repositories.SaleRepository;
 import com.gabrielm.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
