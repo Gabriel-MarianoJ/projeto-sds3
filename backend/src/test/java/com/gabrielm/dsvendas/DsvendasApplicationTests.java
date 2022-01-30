babe77ef5f6d6a684a3dc3a32ad5ddb790e6ddbc
Gabriel Mariano <gabrielmarianodejesus@gmail.com>
2022-01-30 11:24:02 -0300

    Projeto criado

---
+++ backend/src/test/java/com/gabrielm/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielm.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
